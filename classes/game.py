50c2990df46ea89675c75047630cbae8d7cd1d17
brandoncb9906 <brandoncb9906@gmail.com>
2017-06-03 15:44:22 -0600

    Creando paredes, resolves #3

--- classes/game.py
+++ classes/game.py
@@ -6,7 +6,7 @@ class Game:
 		self.__ldata = level_data[level]
 		self.__snake = Snake(self.__ldata["s_size"])
 		size = self.__ldata["m_size"]
-		self.__map = Map(size, size, self.__snake)
+		self.__map = Map(size, size, self.__snake, self.__ldata["walls"])
 
 	def print_map(self):
 		self.__map.print_map()
@@ -19,20 +19,26 @@ class Game:
 
 level_data = [{
 	"m_size": 5,
-	"s_size": 1
+	"s_size": 1,
+	"walls": 1
 }, {
 	"m_size": 6,
-	"s_size": 3
+	"s_size": 3,
+	"walls": 3
 }, {
 	"m_size": 7,
-	"s_size": 3
+	"s_size": 3,
+	"walls": 5
 }, {
 	"m_size": 8,
-	"s_size": 5
+	"s_size": 5,
+	"walls": 7
 }, {
 	"m_size": 9,
-	"s_size": 5
+	"s_size": 5,
+	"walls": 9
 }, {
 	"m_size": 10,
-	"s_size": 7
+	"s_size": 7,
+	"walls": 11
 }]
--- classes/map.py
+++ classes/map.py
@@ -2,11 +2,13 @@ from classes.block import *
 import random
 
 class Map:
-	def __init__(self, width, height, snake):
+	def __init__(self, width, height, snake, walls):
 		self.__width = width
 		self.__height = height
 		self.__snake = snake
+		self.__walls = walls
 		self.__map = self.initialize(width, height)
+		self.create_walls()
 
 	def initialize(self, width, height):
 		the_map = []
@@ -55,3 +57,11 @@ class Map:
 				continue
 
 			return block
+
+	def create_walls(self):
+		for x in range(0, self.__walls):
+			block = self.find_empty()
+			block.set_type(WALL)
+
+
+
